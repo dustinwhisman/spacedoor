02408ecb42959f79406f7147ea404af9723f3087
Dustin Whisman <whisman.dustin@gmail.com>
2020-05-23 13:06:55 -0600

    Add basic configuration and CSS.

---
+++ .eleventy.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function(err, bs) {
+
+        bs.addMiddleware("*", (req, res) => {
+          const content_404 = fs.readFileSync('dist/404/index.html');
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          // Add 404 http status code in request header.
+          // res.writeHead(404, { "Content-Type": "text/html" });
+          res.writeHead(404);
+          res.end();
+        });
+      }
+    },
+    files: [
+      'dist/**/*',
+    ],
+  });
+
+  // eleventyConfig.addPassthroughCopy('favicon.svg');
+  // eleventyConfig.addPassthroughCopy('favicon.ico');
+
+  return {
+    dir: {
+      input: 'src',
+      output: 'dist',
+    },
+  };
+};
