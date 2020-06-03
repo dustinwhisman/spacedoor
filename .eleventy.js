bb6a5771e6b4ede2cf4cc203aab9aecfe0784017
Dustin Whisman <whisman.dustin@gmail.com>
2020-06-02 22:14:50 -0600

    Add Alien dossier pdf.

--- .eleventy.js
+++ .eleventy.js
@@ -23,6 +23,7 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy('src/robots.txt');
   eleventyConfig.addPassthroughCopy('src/favicon.ico');
+  eleventyConfig.addPassthroughCopy('src/dossiers/*');
 
   return {
     dir: {
