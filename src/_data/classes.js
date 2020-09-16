57c6119395832eaad627eb9cbba0ca2cf0f5b0fd
Dustin Whisman <whisman.dustin@gmail.com>
2020-09-16 11:12:56 -0600

    Connect airtable to 11ty and test it on a page.

---
+++ src/_data/classes.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const Airtable = require('airtable');
+const base = new Airtable({
+  apiKey: process.env.API_KEY,
+}).base(process.env.APP_ID);
+
+module.exports = async () => {
+  const records = await base('Classes').select({
+    fields: ['Name', 'Primary Stat', 'Secondary Stat', 'Penalty'],
+    sort: [{
+      field: 'Name',
+      direction: 'asc',
+    }],
+  }).all();
+
+  const classes = records.map((record) => ({
+    ...record.fields,
+    slug: record.fields.Name.toLowerCase().replace(/\s/g, '-'),
+    shorthand: record.fields.Name.replace(/^The\s/, ''),
+  }));
+
+  console.log(classes);
+  return classes;
+};
