dae52bbcce30ed4c13eae5d9f102b0f1024ab3f8
Dustin Whisman <whisman.dustin@gmail.com>
2020-09-23 12:53:38 -0600

    Add page for general feats.

---
+++ src/_data/feats.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const Airtable = require('airtable');
+const base = new Airtable({
+  apiKey: process.env.API_KEY,
+}).base(process.env.APP_ID);
+
+module.exports = async () => {
+  const featRecords = await base('General Feats').select({
+    fields: ['Name', 'Level', 'Description'],
+    sort: [
+      {
+        field: 'Level',
+        direction: 'asc',
+      },
+      {
+        field: 'Order',
+        direction: 'asc',
+      },
+    ]
+  }).all();
+
+  const feats = featRecords.map(r => ({
+    ...r.fields,
+  }));
+
+  return feats;
+};
